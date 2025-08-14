632a0b24382381b95f86d400dc4f5e26d6a3f068
Guilherme Perlasca <perlasca47@gmail.com>
2025-08-14 19:09:10 -0300

    first commit

---
+++ src/main/java/com/example/Screenphrases/ScreenphrasesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Screenphrases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenphrasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenphrasesApplication.class, args);
+	}
+
+}
